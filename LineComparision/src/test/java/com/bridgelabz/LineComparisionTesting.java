693f075ebbe58ab1686e43c6744d073afac2c91c
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-10-15 19:21:40 +0530

    Adding the Custom Exception

--- LineComparision/src/test/java/com/bridgelabz/LineComparisionTesting.java
+++ LineComparision/src/test/java/com/bridgelabz/LineComparisionTesting.java
@@ -7,44 +7,48 @@ public class LineComparisionTesting {
 
     LineComparision obj;
     LineComparision obj1;
-    LineComparision lc= new LineComparision();
-
 
     @Test
-    public void toCheckTheLengthOfLine(){
+    public void toCheckTheLengthOfLine() throws LineLengthCannotBeZeroException {
         obj= new LineComparision(2,4,3,6);
         Assertions.assertEquals(2.24,obj.calculateLength(obj));
     }
 
     @Test
-    public void toCheckTheLengthOfLine1(){
+    public void toCheckTheLengthOfLine1() throws LineLengthCannotBeZeroException {
+        obj= new LineComparision(0,0,0,0);
+        Assertions.assertEquals(0.00,obj.calculateLength(obj));
+    }
+
+    @Test
+    public void toCheckTheLengthOfLine2() throws LineLengthCannotBeZeroException {
          obj= new LineComparision(5,8,2,6);
         Assertions.assertEquals(3.61,obj.calculateLength(obj));
     }
 
     @Test
-    public void  toCheckEqulityOfTwoLineBasedOnEndPoint(){
+    public void  toCheckEqulityOfTwoLineBasedOnEndPoint() throws LineLengthCannotBeZeroException {
          obj = new LineComparision(5,8,2,6);
          obj1=new LineComparision(5,8,2,6);
          Assertions.assertTrue( obj.equality(obj,obj1));
     }
 
     @Test
-    public void  toCheckEqulityOfTwoLineBasedOnEndPoint1(){
+    public void  toCheckEqulityOfTwoLineBasedOnEndPoint1() throws LineLengthCannotBeZeroException {
         obj = new LineComparision(6,7,4,3);
         obj1=new LineComparision(5,8,2,6);
         Assertions.assertFalse( obj.equality(obj,obj1));
     }
 
     @Test
-    public void  toCompareTheTwoLineUsingTheLength(){
+    public void  toCompareTheTwoLineUsingTheLength() throws LineLengthCannotBeZeroException {
         obj = new LineComparision(5,8,2,6);
         obj1=new LineComparision(5,8,2,6);
         Assertions.assertEquals(0,obj.compareLength(obj1,obj));
     }
 
     @Test
-    public void  toCompareTheTwoLineUsingTheLength1(){
+    public void  toCompareTheTwoLineUsingTheLength1() throws LineLengthCannotBeZeroException {
         obj = new LineComparision(2,8,9,4);
         obj1=new LineComparision(5,7,2,3);
         Assertions.assertEquals(-1,obj.compareLength(obj1,obj));
