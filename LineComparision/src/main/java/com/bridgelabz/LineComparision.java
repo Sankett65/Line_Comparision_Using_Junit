e66726cc794ca232295040aec160dd9df0faf721
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-10-15 15:09:30 +0530

    Finding the length of Line

--- LineComparision/src/main/java/com/bridgelabz/LineComparision.java
+++ LineComparision/src/main/java/com/bridgelabz/LineComparision.java
@@ -1,9 +1,42 @@
 package com.bridgelabz;
 
+import java.text.DecimalFormat;
+
 public class LineComparision {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    public LineComparision( ) {
+
+    }
+
+    public LineComparision(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Line Comparison");
+        LineComparision obj =new LineComparision(5,8,2,6);
+        LineComparision obj1 =new LineComparision(5,8,2,6);
+
+
     }
+
+    public double calculateLength(LineComparision lineComparision) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        double powOfx=Math.pow((x2-x1),2);
+        double powOfy=Math.pow((y2-y1),2);
+        double addXAndY=powOfx+powOfy;
+        double length=Math.sqrt(addXAndY);
+        System.out.println("Length of line is : "+df.format(length));
+        double finalAns= Double.parseDouble(df.format(length));
+        return finalAns;
+    }
+
 
 }
---
+++ LineComparision/src/test/java/com/bridgelabz/LineComparisionTesting.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LineComparisionTesting {
+
+    LineComparision obj;
+    LineComparision obj1;
+    LineComparision lc= new LineComparision();
+
+
+    @Test
+    public void toCheckTheLengthOfLine(){
+        obj= new LineComparision(2,4,3,6);
+        Assertions.assertEquals(2.24,obj.calculateLength(obj));
+    }
+
+    @Test
+    public void toCheckTheLengthOfLine1(){
+         obj= new LineComparision(5,8,2,6);
+        Assertions.assertEquals(3.61,obj.calculateLength(obj));
+    }
+
+
+}
